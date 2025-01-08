8a065ef54e83ee16cd885aa3c5b226df8373ae7c
AnkitShakya95 <ankitpatni95@gmail.com>
2025-01-08 22:05:13 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -11,7 +11,7 @@ import yt_dlp
 import m3u8
 import core as helper
 from utils import progress_bar
-from vars import API_ID, API_HASH, BOT_TOKEN
+from vars import API_ID, API_HASH, BOT_TOKEN, AUTH_USER
 from aiohttp import ClientSession
 from pyromod import listen
 from subprocess import getstatusoutput
